1343c745458fa9c6c0653201ee055146ece6b69e
goFrendiAsgard <gofrendiasgard@gmail.com>
2018-04-03 23:39:15 +0700

    try to make markdown renderer

--- setup.js
+++ setup.js
@@ -1,11 +1,15 @@
 const path = require('path')
+const fse = require('fs-extra')
 const {install, uninstall} = require('../../plugin.js')
 
 if (require.main === module) {
   const action = process.argv.length > 2 ? process.argv[2] : 'install'
   const pluginName = path.basename(__dirname)
   if (action === 'install') {
-    install(pluginName)
+    install(pluginName, (error, result) => {
+      // copy markdown library
+      fse.copy('../markdown', '../../public/total-kanban/markdown')
+    })
   } else {
     uninstall(pluginName)
   }
